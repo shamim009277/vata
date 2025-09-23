bc0533183be868580cfdf254c0f891f0002b1797
Towhedul <towhidshamim009@gmail.com>
2025-09-23 16:31:44 +0600

    controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\RawMaterialController;
 use App\Http\Controllers\BusinessSettingController;
 use App\Http\Controllers\SubscriptionPlanController;
 
@@ -23,6 +28,20 @@ Route::middleware(['auth'])->group(function () {
 
     Route::put('/modules/{module}/status', [ModuleController::class, 'updateStatus']);
     Route::resource('modules', ModuleController::class);
+
+    Route::put('/raw-materials/{raw_material}/status', [RawMaterialController::class, 'updateStatus']);
+    Route::resource('raw-materials', RawMaterialController::class);
+
+    Route::put('/items/{item}/status', [ItemController::class, 'updateStatus']);
+    Route::resource('items', ItemController::class);
+
+    Route::put('/suppliers/{supplier}/status', [SupplierController::class, 'updateStatus']);
+    Route::resource('suppliers', SupplierController::class);
+
+    Route::put('/customers/{customer}/status', [CustomerController::class, 'updateStatus']);
+    Route::resource('customers', CustomerController::class);
+
+    Route::resource('purchases', PurchaseController::class);
 });
 
 require __DIR__.'/settings.php';
